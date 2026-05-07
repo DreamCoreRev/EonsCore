c7ed8dca85cda7437370c61b55b750ccfc4a1ef0
Aurøra <azgath.gaming@gmail.com>
2026-05-07 09:55:04 +0200

    Core/Script: New Script for zone_the_wandering_isle

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -444,6 +444,8 @@ class TC_GAME_API WorldObject : public Object, public WorldLocation
         float GetSightRange(WorldObject const* target = nullptr) const;
         bool CanSeeOrDetect(WorldObject const* obj, bool implicitDetect = false, bool distanceCheck = false, bool checkAlert = false) const;
 
+        void SetExplicitSeerGuid(uint64 guid) { m_explicitSeerGuid = guid; }
+
         FlaggedValuesArray32<int32, uint32, StealthType, TOTAL_STEALTH_TYPES> m_stealth;
         FlaggedValuesArray32<int32, uint32, StealthType, TOTAL_STEALTH_TYPES> m_stealthDetect;
 
@@ -666,6 +668,8 @@ class TC_GAME_API WorldObject : public Object, public WorldLocation
         bool CanDetect(WorldObject const* obj, bool ignoreStealth, bool checkAlert = false) const;
         bool CanDetectInvisibilityOf(WorldObject const* obj) const;
         bool CanDetectStealthOf(WorldObject const* obj, bool checkAlert = false) const;
+
+        int32 m_explicitSeerGuid;
 };
 
 namespace Trinity
