d9553661811177cb75b3e1ec6e5152b9a0cbd501
Aurøra <azgath.gaming@gmail.com>
2026-05-16 04:33:06 +0200

    Core/Script: Implemanted System Item Reloading

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -20,6 +20,7 @@
 void AddSC_AccountAchievements();
 void AddSC_AccountCompanions();
 void AddSC_AccountMounts();
+void AddSC_custom_reload_commands();
 void AddLfgSoloScripts();
 void AddSC_premium_account();
 void AddStartGuildScripts();
@@ -32,6 +33,7 @@ void AddCustomScripts()
     AddSC_AccountAchievements();
 	AddSC_AccountCompanions();
 	AddSC_AccountMounts();
+	AddSC_custom_reload_commands();
 	AddLfgSoloScripts();
 	AddSC_premium_account();
 	AddStartGuildScripts();
