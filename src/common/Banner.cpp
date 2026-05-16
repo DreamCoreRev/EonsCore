eb3667d2da43cc19e741de5bd1c52f774ab03edf
Aurøra <azgath.gaming@gmail.com>
2026-05-16 06:05:42 +0200

    Core/Banner: Rename Banner

--- src/common/Banner.cpp
+++ src/common/Banner.cpp
@@ -23,15 +23,14 @@ void Trinity::Banner::Show(char const* applicationName, void(*log)(char const* t
 {
     log(Trinity::StringFormat("{} ({})", GitRevision::GetFullVersion(), applicationName).c_str());
     log(R"(<Ctrl-C> to stop.)" "\n");
-    log(R"( ______                       __)");
-    log(R"(/\__  _\       __          __/\ \__)");
-    log(R"(\/_/\ \/ _ __ /\_\    ___ /\_\ \, _\  __  __)");
-    log(R"(   \ \ \/\`'__\/\ \ /' _ `\/\ \ \ \/ /\ \/\ \)");
-    log(R"(    \ \ \ \ \/ \ \ \/\ \/\ \ \ \ \ \_\ \ \_\ \)");
-    log(R"(     \ \_\ \_\  \ \_\ \_\ \_\ \_\ \__\\/`____ \)");
-    log(R"(      \/_/\/_/   \/_/\/_/\/_/\/_/\/__/ `/___/> \)");
-    log(R"(                                 C O R E  /\___/)");
-    log(R"(https://TrinityCore.org                   \/__/)" "\n");
+    log(R"(    ███████╗ ██████╗ ███╗   ██╗███████╗)");
+	log(R"(    ██╔════╝██╔═══██╗████╗  ██║██╔════╝)");
+	log(R"(    █████╗  ██║   ██║██╔██╗ ██║███████╗)");
+	log(R"(    ██╔══╝  ██║   ██║██║╚██╗██║╚════██║)");
+	log(R"(    ███████╗╚██████╔╝██║ ╚████║███████║)");
+	log(R"(    ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝)");
+	log(R"(                                C O R E)");
+	log(R"(    https://Eons-World.eu)" "\n");
 
     if (logExtraInfo)
         logExtraInfo();
