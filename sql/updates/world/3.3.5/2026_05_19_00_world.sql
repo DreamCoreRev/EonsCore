d5420971f775de0348cb77182732622fb74cea1b
Aurøra <azgath.gaming@gmail.com>
2026-05-19 16:42:56 +0200

    DB: Added Pandaren & Monk Achievement

---
+++ sql/updates/world/3.3.5/2026_05_19_00_world.sql
@@ -0,0 +1,7 @@
+-- achievement_criteria_data
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` = 19204 AND `type` = 21;
+INSERT INTO `achievement_criteria_data`(`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES (19204, 21, 10, 0, '');
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` = 19205 AND `type` = 21;
+INSERT INTO `achievement_criteria_data`(`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES (19205, 21, 0, 25, '');
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` = 19206 AND `type` = 21;
+INSERT INTO `achievement_criteria_data`(`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) VALUES (19206, 21, 0, 26, '');
