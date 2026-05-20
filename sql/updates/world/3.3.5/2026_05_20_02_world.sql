f04bebb69f2f832922c5462538499c1dadca07de
Aurøra <azgath.gaming@gmail.com>
2026-05-20 06:19:37 +0200

    DB: Add Spawn GameObject Stormwind Stockade & Ragefire Chasm

---
+++ sql/updates/world/3.3.5/2026_05_20_02_world.sql
@@ -0,0 +1,7 @@
+-- Planète Azeroth
+
+DELETE FROM `gameobject` WHERE `guid` = 9823021;
+INSERT INTO `gameobject`(`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `StringId`, `VerifiedBuild`) VALUES (9823021, 1660056, 0, 0, 0, 1, 1, -8762.63, 833.59, 97.6351, 3.02718, -0, -0, -0.998364, -0.0571732, 300, 255, 1, '', '', NULL);
+
+DELETE FROM `gameobject` WHERE `guid` = 9823022;
+INSERT INTO `gameobject`(`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `StringId`, `VerifiedBuild`) VALUES (9823022, 1660056, 1, 0, 0, 1, 1, 1798.75, -4404.67, -17.278, 0.129879, -0, -0, -0.0648938, -0.997892, 300, 255, 1, '', '', NULL);
