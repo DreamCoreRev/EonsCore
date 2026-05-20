df1ebe1a9aec4eb4ac65a2cf1505b283fa4ebed4
Aurøra <azgath.gaming@gmail.com>
2026-05-20 03:25:23 +0200

    Core/DB: Add Position WarchiefCommandBoard Alliance & Horde Capitals

---
+++ sql/updates/world/3.3.5/2026_05_20_00_world.sql
@@ -0,0 +1,9 @@
+-- Tableau de l’appel des héros
+
+DELETE FROM `gameobject` WHERE `guid` = 9822959;
+INSERT INTO `gameobject`(`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `StringId`, `VerifiedBuild`) VALUES (9822959, 206111, 0, 0, 0, 1, 1, -8405.36, 634.538, 95.4558, 5.39837, -0, -0, -0.428116, 0.903724, 300, 255, 1, '', '', NULL);
+
+-- Tableau de commandement du chef de guerre
+
+DELETE FROM `gameobject` WHERE `guid` = 9822962;
+INSERT INTO `gameobject`(`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `StringId`, `VerifiedBuild`) VALUES (9822962, 206109, 1, 0, 0, 1, 1, 1878.92, -4516.17, 27.9619, 5.25652, -0, -0, -0.491082, 0.871113, 300, 255, 1, '', '', NULL);
